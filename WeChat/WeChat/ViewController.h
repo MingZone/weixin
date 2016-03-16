59ec487af52880a99bfbe4ead1254fd4d16a7c54
刘汝明 <305255147@qq.com>
2016-03-16 11:26:16 +0800

    初始化项目

---
+++ WeChat/WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by MaxM on 16/3/16.
+//  Copyright © 2016年 ZPLAY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
